147aece379b15f738a799c9e13ab079d3a290ee3
cedanie2 <cedanie2@usf.edu>
2023-11-17 21:34:14 -0500

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+/*
+    Simple JavaScript function to display a confirmation message
+    when the user attempts to reset the data in the table of the 
+    database.db SQLite database
+*/
+function confirm_submit(){
+   alert("Your database will now be reset.");
+}
